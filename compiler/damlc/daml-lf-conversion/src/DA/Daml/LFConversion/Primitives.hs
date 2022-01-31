d925174c28612d48b7799a294a4e9b9e97cfb020
Moisés Ackerman <6054733+akrmn@users.noreply.github.com>
2022-01-31 16:12:13 +0100

    [wip] drop second this argument

--- compiler/damlc/daml-lf-conversion/src/DA/Daml/LFConversion/Primitives.hs
+++ compiler/damlc/daml-lf-conversion/src/DA/Daml/LFConversion/Primitives.hs
@@ -362,13 +362,13 @@ convertPrim _ "ESignatoryInterface" (TCon interface :-> TList TParty) =
     ETmLam (mkVar "this", TCon interface) $
     EExperimental "RESOLVE_VIRTUAL_SIGNATORY"
         (TCon interface :-> TCon interface :-> TList TParty)
-        `ETmApp` EVar (mkVar "this") `ETmApp` EVar (mkVar "this")
+        `ETmApp` EVar (mkVar "this")
 
 convertPrim _ "EObserverInterface" (TCon interface :-> TList TParty) =
     ETmLam (mkVar "this", TCon interface) $
     EExperimental "RESOLVE_VIRTUAL_OBSERVER"
         (TCon interface :-> TCon interface :-> TList TParty)
-        `ETmApp` EVar (mkVar "this") `ETmApp` EVar (mkVar "this")
+        `ETmApp` EVar (mkVar "this")
 
 -- Exceptions
 convertPrim _ "BEAnyExceptionMessage" (TBuiltin BTAnyException :-> TText) =
