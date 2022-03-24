12f13aa6689e28217360b464afdafc63a909c4b2
Nick Chapman <nick.chapman@digitalasset.com>
2022-03-24 11:54:22 +0000

    xbc: run bytecode extract script from root of repo

--- xbc/inspect-bytecode.sh
+++ xbc/inspect-bytecode.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# expect to be run from root of repo
+
 t=tmp # fixed temp dir local to invoking dir, it'll do!
 
 repo=/home/nic/daml #discover automtically from here
@@ -9,5 +11,5 @@ class=xbc/Native
 
 mkdir $t
 (cd $t; cat $jar | jar -x $class)
-find $t -name *.class | xargs javap -p -v > bytecode.text
+find $t -name *.class | xargs javap -p -v > xbc/bytecode.text
 rm -rf $t
